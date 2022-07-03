588c171892647522601dd2298797f18b584bb037
shreephanindra <105204475+shreephanindra@users.noreply.github.com>
2022-07-03 20:00:11 +0530

    first commit

---
+++ api/routes/resolve.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"golang/database"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "cant connect to db"})
+	}
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, 301)
+}
